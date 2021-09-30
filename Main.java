b5c72b5ec1d7a41a16b9f13c9a04f22496b17f87
Srihari Ramasamy Kannan <srihari26692@gmail.com>
2021-09-30 11:20:18 +0000

    java util & java time usage tested

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import java.util.*;
 class Main {
   public static void main(String[] args) {
-    int a[]=new int[]{4, 5, 9, 13, 15, 17};
-    int b[]=new int[]{4, 5, 9, 13, 15, 17};
+  LocalDateTime currentTime = LocalDateTime.now().toLocalDate().atStartOfDay();
+  ZoneOffset localoffset = ZoneOffset.systemDefault().getRules().getOffset(currentTime);
+		Long from = currentTime.toInstant(localoffset).toEpochMilli();
+    System.out.println(from);
+
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTimeZone(TimeZone.getDefault());
+		calendar.set(Calendar.YEAR, 2021);
+		calendar.set(Calendar.MONTH, 9 - 1);
+		calendar.set(Calendar.DAY_OF_MONTH, 30);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+    System.out.println(calendar.getTimeInMillis());
   }
 }
