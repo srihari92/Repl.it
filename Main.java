cdbb1e016fa53bd0fed92be285280ce9fe4045ef
Sriharir_k <>
2021-09-30 11:01:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+class Main {
+  public static void main(String[] args) {
+    int a[]=new int[]{4, 5, 9, 13, 15, 17};
+    int b[]=new int[]{4, 5, 9, 13, 15, 17};
+  }
+}
